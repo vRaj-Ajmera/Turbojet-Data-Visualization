e739001db40bad3150c4288454de40a1210723ea
orened <152335761+orened@users.noreply.github.com>
2024-03-05 10:31:52 -0600

    Update turbine.py

--- utils/turbine.py
+++ utils/turbine.py
@@ -6,17 +6,33 @@ class TurbineModel:
     """
 
     @staticmethod
-    def calculate_total_enthalpy_out(ht_in, ht_out_ideal, eff):
-        """
-        Calculate the total enthalpy at the outlet of the turbine.
+from sympy import *
+#cp and R need to be defined for the Air Gas mixture
 
-        Parameters:
-        - ht_in (float): Total enthalpy at the inlet of the turbine.
-        - ht_out_ideal (float): Ideal total enthalpy at the outlet of the turbine.
-        - eff (float): Efficiency of the turbine.
+def Turbine(Airvel_4, h4, T4, P4, cp, R, massdot):
 
-        Returns:
-        - float: Total enthalpy at the outlet of the turbine.
-        """
-        ht_out = ht_in + eff * (ht_in - ht_out_ideal)
-        return ht_out
+    # Define symbols
+    h45, T45, P45, workdotout4to45, Airvel_45 = symbols('h45, T45, P45, workdotout45, Airvel_45')
+    h5, T5, P5, workdotout45to5, Airvel_5 = symbols('h5, T5, P5, workdotout5, Airvel_5')
+
+    # Equations HPT to LPT (4 to 4.5)
+    enthalpy4to45 = Eq(h45, cp * T45)
+    energy4to45 = Eq(workdotout4to45, massdot * ((0.5 * (Airvel_4**2 - Airvel_45**2)) + h4 - h45))    #Assume well insulated system implies isentropic process
+    entropy4to45 = Eq(0, cp * log(T45 / T4) - R * log(P45 / P4))  #Ideal gas assumption, The alternative will be a table lookup interpolation
+    powerout4to45 = workdotout4to45
+
+    #Equations LPT to Duct (4.5 to 5)
+    enthalpy45to5 = Eq(h5, cp * T5)
+    energy45to5 = Eq(workdotout45to5, massdot * ((0.5 * (Airvel_45**2 - Airvel_5**2)) + h45 - h5))    #Assume well insulated system implies isentropic process
+    entropy45to5 = Eq(0, cp * log(T5 / T45) - R * log(P5 / P45))  #Ideal gas assumption, The alternative will be a table lookup interpolation
+    powerout45to5 = workdotout45to5
+
+    # Solve the system of equations
+    solution = solve((enthalpy4to45, energy4to45, entropy4to45, enthalpy45to5, energy45to5, entropy45to5 ), (h5, T5, P5,workdotout4to45,workdotout45to5))
+
+    # Retrieve values from the solution
+    h5 = solution[h5]
+    T5 = solution[T5]
+    P5 = solution[P5]
+    poweroutturbine = solution[workdotout4to45]+ solution[workdotout45to5]
+    return h5,T5,P5,poweroutturbine
