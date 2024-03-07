e1ab89c972d8984108645d83f6fd0d34db0fdab6
orened <152335761+orened@users.noreply.github.com>
2024-03-07 12:47:46 -0600

    Update turbine.py

--- utils/turbine.py
+++ utils/turbine.py
@@ -28,3 +28,30 @@ def TVel2h_total(t, v):
     # Total enthalpy or stagnation enthalpy v is velocity magnitude
     h_t = h + .5*v**2
     return h_t
+def calculate_power(massdot, h4, h5):
+    """
+    Calculate power using the given equation.
+
+    Parameters:
+        massdot (float): Mass flow rate.
+        h4 (float): Stagnation Enthalpy at state 4.
+        h5 (float): Enthalpy at state 5.
+
+    Returns:
+        float: Calculated power.
+    """
+    return massdot * (h4 - h5)
+
+def compressor_power(power_out_turbine, n_m=0.95):     #the efficiency paramater in the book is given as .95
+    """
+    Calculates the power delivered to the compressor based on the efficiency parameter
+    and the power output of the turbine.
+
+    Args:
+    power_out_turbine (float): Power output of the turbine.
+    n_m (float, optional): Efficiency parameter of the shaft. Defaults to 0.95.
+
+    Returns:
+    float: Power output to the compressor.
+    """
+    return n_m * power_out_turbine
