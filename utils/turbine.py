63c31715bd79661aba4c570ce38267d8382b88b9
orened <152335761+orened@users.noreply.github.com>
2024-03-07 11:51:00 -0600

    Update turbine.py
    Added a stagnation enthalpy calculator

--- utils/turbine.py
+++ utils/turbine.py
@@ -1,34 +1,31 @@
 # turbine_model.py
-from sympy import *
+pip install cantera
+import cantera as ct
 class TurbineModel:
     """
     Class for modeling turbine behavior.
     """
-    #cp and R need to be defined for the Air Gas mixture
-    def Turbine(Airvel_4, h4, T4, P4, cp, R, massdot):
 
-        # Define symbols
-        h45, T45, P45, workdotout4to45, Airvel_45 = symbols('h45, T45, P45, workdotout45, Airvel_45')
-        h5, T5, P5, workdotout45to5, Airvel_5 = symbols('h5, T5, P5, workdotout5, Airvel_5')
+def TPV2h(t, p, v):
+    """
+    Calculate the enthalpy of air at given temperature and pressure.
 
-        # Equations HPT to LPT (4 to 4.5)
-        enthalpy4to45 = Eq(h45, cp * T45)
-        energy4to45 = Eq(workdotout4to45, massdot * ((0.5 * (Airvel_4**2 - Airvel_45**2)) + h4 - h45))    #Assume well insulated system implies isentropic process
-        entropy4to45 = Eq(0, cp * log(T45 / T4) - R * log(P45 / P4))  #Ideal gas assumption, The alternative will be a table lookup interpolation
-        powerout4to45 = workdotout4to45
+    Parameters:
+        t (float): The temperature in Kelvin.
+        p (float): The pressure in Pascals.
+        v (float): The air velocity magnitude in m/s.
 
-        #Equations LPT to Duct (4.5 to 5)
-        enthalpy45to5 = Eq(h5, cp * T5)
-        energy45to5 = Eq(workdotout45to5, massdot * ((0.5 * (Airvel_45**2 - Airvel_5**2)) + h45 - h5))    #Assume well insulated system implies isentropic process
-        entropy45to5 = Eq(0, cp * log(T5 / T45) - R * log(P5 / P45))  #Ideal gas assumption, The alternative will be a table lookup interpolation
-        powerout45to5 = workdotout45to5
+    Returns:
+        float: The specific stagnation enthalpy of air in J/kg.
+    """
+    # Create a Solution object for air
+    air = ct.Solution('air.yaml')
 
-        # Solve the system of equations
-        solution = solve((enthalpy4to45, energy4to45, entropy4to45, enthalpy45to5, energy45to5, entropy45to5 ), (h5, T5, P5,workdotout4to45,workdotout45to5))
+    # Set the temperature and pressure
+    air.TP = temperature, pressure
 
-        # Retrieve values from the solution
-        h5 = solution[h5]
-        T5 = solution[T5]
-        P5 = solution[P5]
-        poweroutturbine = solution[workdotout4to45]+ solution[workdotout45to5]
-        return h5,T5,P5,poweroutturbine
+    # Calculate the enthalpy
+    h = air.enthalpy_mass
+    # Total enthalpy or stagnation enthalpy v is velocity magnitude
+    h_t = h + .5*v**2
+    return h_t
