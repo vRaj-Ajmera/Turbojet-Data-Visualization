1f444aa6ea105773e2d8bfa00b8088984be33b18
MattWeippert <91439463+MattWeippert@users.noreply.github.com>
2024-03-17 12:35:43 -0500

    Update ambient_inlet.py
    Added some initial equations to this file, will return with more later today.

--- utils/ambient_inlet.py
+++ utils/ambient_inlet.py
@@ -1,22 +1,32 @@
 # ambient_inlet.py
 
 class AmbientInletModel:
-    """
-    Class for modeling ambient and inlet conditions.
-    """
 
-    @staticmethod
-    def calculate_drag(W_in, V_out, g):
-        """
-        Calculate drag force based on mass flow rate, outlet velocity, and gravity.
+    # isentropic efficiency of the diffuser eta_d
+    def findIsEff_RatioMethod(tau_r, pi_d, gamma):
+        numerator = ((tau_r * pi_d) ** ((gamma - 1)/gamma)) - 1
+        denominator = tau_r - 1
+        return numerator/denominator
 
-        Parameters:
-        - W_in (float): Mass flow rate into the system.
-        - V_out (float): Outlet velocity.
-        - g (float): Acceleration due to gravity.
+    def findIsEff_EnthMethod(h_t2s, h_t0, h_0):
+        numerator = h_t2s - h_0
+        denominator = h_t0 - h_0
+        return numerator/denomincator
 
-        Returns:
-        - float: Calculated drag force.
-        """
-        drag_force = W_in * V_out / g
-        return drag_force
+    def findIsEff_TempMethod(T_t2s, T_t0, T_0):
+        numerator = T_t2s - T_0
+        denominator = T_t0 - T_0
+        return numerator/denomincator
+
+    # Finding the total pressure ratio pi_d
+    def findPRessureRatio(M_0, pi_dmax):
+        if M_0 <= 1:
+            return 1 * pi_dmax
+        elif M_0 > 1 and M_0 < 5:
+            eta_r = 1 - (0.075*(M_0 - 1)**1.35)
+            return eta_r * pi_dmax
+        else:
+            eta_r = 800 / (M_0**4 + 935)
+            return eta_r * pi_dmax
+
+    
