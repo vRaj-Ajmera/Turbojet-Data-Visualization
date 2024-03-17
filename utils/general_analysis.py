09ca924c65ae6ed624bc3347d7dd36ce16c8d27a
MattWeippert <91439463+MattWeippert@users.noreply.github.com>
2024-03-17 16:32:07 -0500

    Update general_analysis.py
    Added more equations

--- utils/general_analysis.py
+++ utils/general_analysis.py
@@ -19,3 +19,45 @@ class GeneralAnalysis:
     def corrMassFlowRate(m_dot_i, delta_i, theta_i):
         numerator = m_dot_i * (theta_i**0.5)
         return numerator / delta_i
+
+    # Calculating mass-flow parameter MFP
+    def massFlowParameter(m_ci, A_i, p_ref, T_ref):
+        numerator = m_ci * (T_ref**0.5)
+        denominator = A_i * p_ref
+        returm numerator/denominator
+
+    # Corrected Engine Speed N_ci
+    def corrEngineSpeed(N, theta_i):
+        return N/(theta_i**0.5)
+
+    # Corrected Thrust Fc
+    def corrThrust(F, delta_0):
+        return F/delta_0
+
+    # Corrected Thrust specific fuel consumption S_c
+    def corrTSFC(S, theta_0):
+        return S/(theta_0**0.5)
+
+    # Corrected fuel mass flow rate m_dot_fc
+    def corrFMFR(m_dot_f, delta_2, theta_2)
+        denominator = delta_2 * (theta_2**0.5)
+        return m_dot_f / denominator
+
+    # Specific m_dot_c2 calculation (gas generator)
+    def calcMc2(T_4, T_2, pi_c, C_1):
+        return C_1 * (pi_c / ((T_4/T_2)**0.5))
+        
+    # Turbine Characteristic: Capital Eta
+    def capitalEta_i(gamma_i):
+        return (gamma_i**0.5) * (2/(gamma_i + 1))**((gamma_i + 1)/(2*(gamma_i - 1)))
+
+    # Corrected mass flow rate per unit area at station 4
+    def calc_m4(p_4, A_4, gamma_i, T_4, R_4):
+        numerator = p_4 * A_4 * capitalEta(gamma_i)
+        denominator = (T_4**0.5) * (R_4**0.5)
+        return numerator/denominator
+
+    
+
+    
+
