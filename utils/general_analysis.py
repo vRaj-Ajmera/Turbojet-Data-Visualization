bf0fe0fd1114dafeaa4312da52d87f2d0c852836
MattWeippert <91439463+MattWeippert@users.noreply.github.com>
2024-03-20 14:53:25 -0500

    Update general_analysis.py
    Almost have a fully working model for the thrust

--- utils/general_analysis.py
+++ utils/general_analysis.py
@@ -7,88 +7,124 @@
 
 class GeneralAnalysis:
 
-    # Calculating referece pressure ratio at any given station delta_i
-    def pressureRef(p_ref, p)
-        return p/p_ref
+    # Inputs: M_0, T_0, P_0, T_t4, P_9
+    # Design Constants: pi_d_max, pi_b, pi_t, pi_n, tau_t, eta_c, eta_b, eta_m, gamma_c, gamma_t, c_pc, c_pt, h_br
+    # Reference Values: M_0R, T_0R, P_0R, tau_rR, pi_rR, T_t4R, pi_dR, pi_cR, tau_cR
+    # Outputs: F, m_dot, f, S, eta_p, eta_t, eta_o, pi_d, pi_c, tau_c, f, M_9, N/N_r
 
-    # Calculating reference temperature ratio at any given station theta_i
-    def temperatureRef(T_ref, T)
-        return T/Tref
+    # Note: For maps concerning reasonable values of pressure ratios and efficiencies for various types of aircraft, check page 363 of textbook
 
-    # Calculating corrected mass flow rate m_dot_c at any station 'i'
-    def corrMassFlowRate(m_dot_i, delta_i, theta_i):
-        numerator = m_dot_i * (theta_i**0.5)
-        return numerator / delta_i
+    # Start with outputs and work backwards
 
-    # Calculating mass-flow parameter MFP
-    def massFlowParameter_Simple(m_ci, A_i, p_ref, T_ref):
-        numerator = m_ci * (T_ref**0.5)
-        denominator = A_i * p_ref
-        returm numerator/denominator
+    def TSFC(a_0, g_c, f, V_9, M_0, R_t, R_c, T_9, T_0, P_0, P_9, gamma_c):
+        return (a_0/g_c)*(((1 + f)*(V_9/a_0)) - M_0 + ((1 + f)*(R_t/R_c)*((T_9/T_0)/(V_9/a_0))*((1 - (P_0/P_9))/gamma_c)))
 
-    def massFlowParameter_Mach(M, gamma, g_c, R):
-        numerator = M*(((gamma*gc)/R)**0.5)
-        denominator = (1 + (((gamma - 1)/2)*M)**2)**((gamma + 1)/(2*(gamma - 1)))
-        return numerator/denominator
+    def thrust(tsfc, m_dot):
+        return tsfc*m_dot
 
-    # Corrected Engine Speed N_ci
-    def corrEngineSpeed(N, theta_i):
-        return N/(theta_i**0.5)
+    def S(tsfc, f):
+        return (f/tsfc)
 
-    # Corrected Thrust Fc
-    def corrThrust(F, delta_0):
-        return F/delta_0
+    def eta_T(a_0, f, V_9, M_0, g_c, h_pR):
+        numerator = (a_0**2)*(((1 + f)*((V_9/a_0)**2)) - (M_0**2))
+        denominator = 2*g_c*f*h_pR
+        return numerator / denominator
 
-    # Corrected Thrust specific fuel consumption S_c
-    def corrTSFC(S, theta_0):
-        return S/(theta_0**0.5)
+    def eta_P(a_0, f, V_0, M_0, g_c, tsfc):
+        numerator = 2*g_c*V_0*tsfc
+        denominator = (a_0**2)*(((1 + f)*((V_0/a_0)**2)) - (M_0**2))
+        return numerator / denominator
 
-    # Corrected fuel mass flow rate m_dot_fc
-    def corrFMFR(m_dot_f, delta_2, theta_2)
-        denominator = delta_2 * (theta_2**0.5)
-        return m_dot_f / denominator
+    def n_over_nr(T_0, tau_r, pi_c, gamma_t, T_0R, tau_rR, pi_cR):
+        numerator = T_0*tau_r*(pi_c**((gamma_c - 1)/gamma_c)) - 1
+        denominator = T_0R*tau_rR*(pi_cR**((gamma_c - 1)/gamma_c)) - 1
+        return (numerator / denominator)**0.5
 
-    # Specific m_dot_c2 calculation (gas generator)
-    def calcMc2(T_4, T_2, pi_c, C_1):
-        return C_1 * (pi_c / ((T_4/T_2)**0.5))
+    # Find design constants
+
+    def calc_R_c(gamma_c, c_pc):
+        return ((gamma_c - 1)/gamma_c)*c_pc
+
+    def calc_R_t(gamma_t, c_pt):
+        return ((gamma_t - 1)/gamma_t)*c_pt
+
+    def calc_a_0(gamma_c, R_c, g_c, T_0):
+        return (gamma_c*R_c*g_c*T_0)**0.5
+
+    def calc_V_0(a_0, M_0):
+        return a_0*M_0
+
+    def calc_tau_r(gamma_c, M_0):
+        return 1 + (((gamma_c - 1)/2) * (M_0**2))
+
+    def calc_pi_r(tau_r, gamma_c):
+        return tau_r**(gamma_c/(gamma_c - 1))
+
+    def calc_eta_r(M_0):
+        if M_0 <= 1:
+            return 1
+        else:
+            return 1 - (0.075*(M_0 - 1)**1.35)
+
+    def calc_pi_d(pi_d_max, eta_r):
+        return pi_d_max * eta_r
+
+    def calc_T_t2(T_0, tau_r):
+        return T_0*tau_r
+
+    def calc_tau_c(tau_cR, T_t4, T_t2, T_t4R, T_t2R):
+        return 1 + (tau_cR - 1)*((T_t4/T_t2)/(T_t4R/T_t2R))
+
+    def calc_pi_c(eta_c, tau_c, gamma_c):
+        return (1 + eta_c*(tau_c - 1))**(gamma_c/(gamma_c - 1))
+
+    def calc_tau_lambda(c_pt, T_t4, c_pc, T_0):
+        return (c_pt*T_t4)/(c_pc*T_0)
+
+    def calc_f(tau_lambda, tau_r, tau_c, h_pR, eta_b, c_p, T_0):
+        numerator = tau_lambda - (tau_r*tau_c)
+        denominator = (h_pR*eta_b)/(c_p*T_0 - tau_lambda)
+        return numerator / denominator
+
+    def calc_m_dot(m_dot_R, P_0, pi_r, pi_d, pi_c, P_0R, pi_rR, pi_dR, pi_cR, T_t4, T_t4R):
+        numerator = P_0*pi_r*pi_d*pi_c
+        denominator = P_0R*pi_rR*pi_dR*pi_cR
+        return m_dot_R * (numerator / denominator) * ((T_t4R/T_t4)**0.5)
+
+    def calc_P9Rat(P_0, P_9, pi_r, pi_d, pi_c, pi_b, pi_t, pi_n):
+        return (P_0/P_9)*pi_r*pi_d*pi_c*pi_b*pi_t*pi_n
+
+    def calc_M_9(gamma_t, P9rat, tau_t):
+        return ((2/(gamma_t - 1))*((P9rat**((gamma_t - 1)/tau_t)) - 1))**0.5
+
+    def calc_T9T0Rat(T_t4, tau_t, P9rat, gamma_t):
+        return (T_t4*tau_t)/(P9rat**((gamma_t - 1)/gamma_t))
+
+    def calc_V0a0Rat(M_9, gamma_t, R_t, T_9, gamma_c, R_c, T_0):
+        return M_9*(((gamma_t*R_t*T_9)/(gamma_c*R_c*T_0))**0.5)
+
+    # Combine all of them to make complete equations from clear independent variables
+
+    def calculateTSFC(M_0, T_0, P_0, T_t4, P_9, g_c, gamma_c, c_pc, gamma_t, c_pt, pi_d_max, tau_cR, T_t4R, tau_rR, eta_c, h_pR, eta_b):
+        # Calculate R_c ad R_t
+        R_c = calc_R_c(gamma_c, c_pc)
+        R_t = calc_R_t(gamma_t, c_pt)
+
+        # Calculate a_0
+        a_0 = calc_a_0(gamma_c, R_c, g_c, T_0)
+
+        # Calculate various pressure ratios and efficiencies needed later
+        tau_r = calc_tau_r(gamma_c, M_0)
+        pi_r = calc_pi_r(tau_r, gamma_c)
+        eta_r = calc_eta_r(M_0)
+        pi_d = calc_pi_d(pi_d_max, eta_r)
+        T_t2 = calc_T_t2(T_0, tau_r)
+        tau_c = calc_tau_c(tau_cR, T_t4, T_t2, T_t4R, calc_T_t2(T_0, tau_rR))
+        pi_c = calc_pi_c(eta_c, tau_c, gamma_c)
+        tau_lambda = calc_tau_lambda(c_pt, T_t4, c_pc, T_0)
+
+        # Calculate f
+        f = calc_f(tau_lambda, tau_r, tau_c, h_pR, eta_b, c_p, T_0)
         
-    # Turbine Characteristic: Capital Eta
-    def capitalEta_i(gamma_i):
-        return (gamma_i**0.5) * (2/(gamma_i + 1))**((gamma_i + 1)/(2*(gamma_i - 1)))
-
-    # Corrected mass flow rate per unit area at station 4
-    def calc_m4(p_4, A_4, gamma_i, T_4, R_4):
-        numerator = p_4 * A_4 * capitalEta(gamma_i)
-        denominator = (T_4**0.5) * (R_4**0.5)
-        return numerator/denominator
-
-    # Corrected mass flow rate per unit area station 8
-    def calc_m8(p_8, A_8, T_8, M_8, gamma, g_c, R):
-        return (p_8 * A_8 * massFlowParameter_Mach(M_8, gamma, g_c, R))/(T_8**0.5)
-
-    # Pressure ratio between stage 4 and stage 8 where eta_t is turbine efficiency
-    def pressureRatio4_8(T_4, T_8, eta_t, gamma_t):
-        return (1 - ((1 - (T_8/T_4))/turb_eff))**(gamma_t/(gamma_t - 1))
-
-    def temperatureRatio4_8(A_4, A_8, M_8, gamma, R, T_4, T_8, eta_t)
-        numerator = A_8 * massFlowParameter_Mach(M_8, gamma, g_c, R) * pressureRatio4_8(T_4, T_8, eta_t, gamma)
-        denominator = A_4 * (capitalEta(gamma)/(R**0.5))
-        return (numerator/denominator)**2
-
-    # Root of the temperature ratio as a function of Area, Mach number, and capital Eta
-    def tempPressTurbineRel(A_4, A_8, M_8, gamma, R)
-        numerator = A_8 * massFlowParameter_Mach(M_8, gamma, g_c, R)
-        denominator = A_4 * (capitalEta(gamma)/(R**0.5))
-        return numerator/denominator
-
-    # Calculate compressor pressure ratio
-    def compressorPressureRatio(T_4, theta_0, K, gamma_c):
-        return (1 + (T_4/theta_0)*K)**(gamma_c/(gamma_c - 1))
-
-    # Corrected mass flow rate through compressor
-    def corrMassFlowCompr(theta_0, T_4, K_1, K_2)
-        return ((theta_0/T_4)**0.5) * ((1 + (T_4/theta_0)*K_1)**(gamma_c/(gamma_c - 1))) * K_2
-
-
-        
-
+        # Finally plug everything in
+        tsfc = TSFC(a_0, g_c, f, V_9, M_0, R_t, R_c, T_9, T_0, P_0, P_9, gamma_c)
